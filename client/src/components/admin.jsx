fb12aa07898041f8766586329095f7264edabf81
Ankit Kumar <143013601+ankitgithub12@users.noreply.github.com>
2025-12-16 22:17:50 +0530

    Update admin.jsx

--- client/src/components/admin.jsx
+++ client/src/components/admin.jsx
@@ -44,7 +44,7 @@ const Admin = () => {
     total: 0
   });
 
-  const API_BASE_URL = 'https://sitaram-inter-college.onrender.com/';
+  const API_BASE_URL = 'https://sitaram-inter-college.onrender.com';
 
   // Image Modal Functions
   const handleViewImage = (imageUrl, fileName) => {
