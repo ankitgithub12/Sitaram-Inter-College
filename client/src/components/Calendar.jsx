51b03c0008885d9c144114eb5994668e17287a92
ankitgithub12 <ankit639520@gmail.com>
2026-05-09 22:28:03 +0530

    Fix incorrect import path in Calendar.jsx

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { apiUrl } from './lib/config';
+import { apiUrl } from '../lib/config';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
